9275e8ae88e3f12aac709b3225cb167c68c38d31
maxwellbenton <maxwellbenton@gmail.com>
2019-02-20 14:44:13 -0500

    updates deprecated style

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe NotesController, type: :controller do
   fixtures :users
   describe 'post create' do
     it "can't create a note if you're not logged in" do
-      post :create, note: {content: 'hush', visible_to: ''}
+      post :create, params: {  note: {content: 'hush', visible_to: ''} }
       assert_redirected_to '/'
     end
     it "can create a note if you're logged in" do
       alice = users(:alice)
       content = 'secret message'
       session[:user_id] = alice.id
-      post :create, note: {content: content, visible_to: ''}
+      post :create, params: { note: {content: content, visible_to: ''} }
       assert_redirected_to '/'
       note = Note.last
       assert note.content == content
@@ -24,14 +24,14 @@ RSpec.describe NotesController, type: :controller do
     it "can update your own notes" do
       alice, beth = users(:alice), users(:beth)
       session[:user_id] = beth.id
-      
+
       content = 'oh so secret'
-      post :create, note: {content: content, visible_to: ''}
+      post :create, params: { note: {content: content, visible_to: ''} }
       note_id = Note.last.id
       assert Note.find(note_id).content == content
 
       new_content = 'a different secret'
-      post :update, id: note_id, note: {content: new_content, visible_to: 'alice'}
+      post :update, params: { id: note_id, note: {content: new_content, visible_to: 'alice'} }
       assert_redirected_to '/'
       note = Note.find(note_id)
       assert note.content == new_content
