a376c370c848a4b3ab982f4822cb036b08751f35
maxwellbenton <maxwellbenton@gmail.com>
2019-09-26 13:23:13 -0400

    removes space from test to align with instructions

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Note, type: :model do
   describe 'visible_to=' do
     it "takes a comma separated list of names, trims spaces, and makes those users readers" do
       note = Note.new      
-      note.visible_to = 'alice, beth'      
+      note.visible_to = 'alice,beth'      
       assert note.readers == [users(:alice), users(:beth)]
     end
   end
@@ -13,7 +13,7 @@ RSpec.describe Note, type: :model do
   describe "visible_to" do
     it "returns reader names as a comma-separated string" do
       note = Note.new readers: [users(:alice), users(:beth)]
-      assert note.visible_to == 'alice, beth'
+      assert note.visible_to == 'alice,beth'
     end
   end
 end
